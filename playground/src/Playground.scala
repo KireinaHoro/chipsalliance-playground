26776f046d00c43d4d5e719b55cd77e86648bf12
KireinaHoro <i@jsteward.moe>
2021-04-09 01:07:58 +0800

    add basic codesign harness

---
+++ playground/src/Playground.scala
@@ -0,0 +1,29 @@
+import barstools.tapeout.transforms.GenerateTopAndHarness
+import chipyard._
+import firrtl.options._
+import firrtl.stage.FirrtlFileAnnotation
+import freechips.rocketchip.stage._
+
+import java.nio.file._
+import java.io._
+import scala.reflect.io.Directory
+
+object Runner extends App {
+  val build = Paths.get("out/generated-src")
+  if (Files.exists(build)) {
+    val dir = new Directory(new File(build.toString))
+    dir.deleteRecursively()
+  }
+  Files.createDirectories(build)
+
+  val topClass = classOf[CodesignHarness]
+  val configClass = classOf[CodesignVCU118Config]
+  val outputBaseName = f"${topClass.getName}.${configClass.getName}"
+
+  Generator.stage.transform(Seq(
+    new TargetDirAnnotation(build.toString),
+    new TopModuleAnnotation(topClass),
+    new ConfigsAnnotation(Seq(configClass.getName)),
+    new OutputBaseNameAnnotation(outputBaseName),
+  ))
+}
